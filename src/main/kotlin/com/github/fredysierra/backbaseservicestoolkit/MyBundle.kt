10cf18da6937864f971f3538a70a89626e6e87e8
GitHub Action <action@github.com>
2021-10-05 13:22:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/fredysierra/backbaseservicestoolkit/MyBundle.kt
+++ src/main/kotlin/com/github/fredysierra/backbaseservicestoolkit/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fredysierra.backbaseservicestoolkit
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/fredysierra/backbaseservicestoolkit/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fredysierra.backbaseservicestoolkit.services
+
+import com.github.fredysierra.backbaseservicestoolkit.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
