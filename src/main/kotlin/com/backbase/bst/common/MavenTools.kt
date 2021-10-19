244e5c3e50046d8110a4dceb1ac249641084ffb2
Fredy Sierra <fredy@backbase.com>
2021-10-19 15:08:51 +0200

    Add persistence feature (#3)
    * Add persistence feature
    
    * Update CHANGELOG.md file

---
+++ src/main/kotlin/com/backbase/bst/common/MavenTools.kt
@@ -0,0 +1,48 @@
+package com.backbase.bst.common
+
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.idea.maven.dom.MavenDomUtil
+import org.jetbrains.idea.maven.dom.model.MavenDomDependencies
+import org.jetbrains.idea.maven.dom.model.MavenDomDependency
+import org.jetbrains.idea.maven.model.MavenCoordinate
+import org.jetbrains.idea.maven.model.MavenId
+import org.jetbrains.idea.maven.utils.MavenUtil
+import org.jetbrains.idea.maven.utils.actions.MavenActionUtil
+
+object MavenTools {
+
+    fun createDomDependency(
+        dependencies: MavenDomDependencies?,
+        editor: Editor?,
+        id: MavenCoordinate
+    ): MavenDomDependency? {
+        val dep = MavenDomUtil.createDomDependency(dependencies!!, editor)
+        dep.groupId.stringValue = id.groupId
+        dep.artifactId.stringValue = id.artifactId
+        return dep
+    }
+
+    fun writeDependenciesOnPom(project: Project, file: VirtualFile, dataContext: DataContext, dependencies: List<MavenId>) {
+        val mavenModel = MavenDomUtil
+            .getMavenDomProjectModel(project, file)
+        val editor = dataContext.getData(CommonDataKeys.EDITOR)
+        dependencies.forEach{dependency -> createDomDependency(mavenModel!!.dependencies, editor, dependency)}
+    }
+
+    fun findPomXml(dataContext: DataContext): VirtualFile? {
+        var file = CommonDataKeys.VIRTUAL_FILE.getData(dataContext) ?: return null
+        if (file.isDirectory) {
+            file = MavenUtil.streamPomFiles(MavenActionUtil.getProject(dataContext), file).findFirst().orElse(null)
+            if (file == null) return null
+        }
+        val manager = MavenActionUtil.getProjectsManager(dataContext) ?: return null
+        manager.findProject(file) ?: return null
+        return file
+    }
+
+
+}
---
+++ src/test/testData/addingJavaAnnotation/com/backbase/test2w/Application.java
@@ -0,0 +1,17 @@
+package com.backbase.test2w;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
