c20b78fa3d0ff0863b5d4e18187e8c0468da754e
Fredy Sierra <fredy@backbase.com>
2021-12-08 10:13:53 +0100

    fix: Fix validation when defining and event (#8)

--- src/main/kotlin/com/backbase/bst/actions/DefineEventDialog.kt
+++ src/main/kotlin/com/backbase/bst/actions/DefineEventDialog.kt
@@ -5,12 +5,15 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.layout.CellBuilder
 import com.intellij.ui.layout.panel
 import javax.swing.JComponent
 
 class DefineEventDialog(project : Project) : DialogWrapper(project, true) {
 
     var  mainPanel : DialogPanel? = null
+    var textField : CellBuilder<JBTextField>? = null
 
     init {
         title = BackbaseBundle.message("action.add.define.event.dialog.title")
@@ -22,9 +25,11 @@ class DefineEventDialog(project : Project) : DialogWrapper(project, true) {
     override fun createCenterPanel(): JComponent? {
         mainPanel = panel {
             row {
-                 cell {
+
+                cell {
                      label(BackbaseBundle.message("action.add.define.event.dialog.event.name"))
-                     textField(::eventName).focused()
+                     textField = textField(::eventName)
+                     textField!!.focused()
                  }
             }
         }
@@ -33,7 +38,7 @@ class DefineEventDialog(project : Project) : DialogWrapper(project, true) {
     }
 
     override fun doValidate(): ValidationInfo? {
-        if (eventName == "undefined"){
+        if (textField!!.component.text == ""){
             return ValidationInfo("Event Name cannot be empty")
         }
 
