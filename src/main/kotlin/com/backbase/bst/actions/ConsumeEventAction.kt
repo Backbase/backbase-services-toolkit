e07bb7463ae9dd19b30551a196bf2ddcb031ff6b
Dhananjay Singh <dhananjay@backbase.com>
2022-03-30 10:03:09 +0200

    Events package and class generation fix

--- src/main/kotlin/com/backbase/bst/actions/ConsumeEventAction.kt
+++ src/main/kotlin/com/backbase/bst/actions/ConsumeEventAction.kt
@@ -8,7 +8,6 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
@@ -25,23 +24,27 @@ import java.io.File
 import java.nio.file.Paths
 
 
-class ConsumeEventAction : DumbAwareAction(){
+class ConsumeEventAction : DumbAwareAction() {
 
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = MavenActionUtil.getProject(e.dataContext) ?: return
 
-        val eventClass = JavaPsiFacade.getInstance(project).findClass("com.backbase.buildingblocks.persistence.model.Event",
-            GlobalSearchScope.allScope(project))
+        val eventClass = JavaPsiFacade.getInstance(project).findClass(
+            "com.backbase.buildingblocks.persistence.model.Event",
+            GlobalSearchScope.allScope(project)
+        )
 
-        fun getEntireName (psiClass: PsiClass) : String {
+        fun getEntireName(psiClass: PsiClass): String {
             val packageName = (psiClass.containingFile as PsiJavaFile).packageName
             return "$packageName.${psiClass.name}"
         }
 
-        val events = ClassInheritorsSearch.search(eventClass!!, GlobalSearchScope.allScope(project),
-            true, true, true)
-            .map { getEntireName(it)}.toList()
+        val events = ClassInheritorsSearch.search(
+            eventClass!!, GlobalSearchScope.allScope(project),
+            true, true, true
+        )
+            .map { getEntireName(it) }.toList()
 
         val consumeEventDialog = ConsumeEventDialog(project, events)
 
@@ -60,7 +63,7 @@ class ConsumeEventAction : DumbAwareAction(){
     }
 
     private fun addEventHandler(e: AnActionEvent, project: Project, eventClass: String) {
-        val justClass = eventClass.substringAfterLast("." )
+        val justClass = eventClass.substringAfterLast(".")
         val eventHandlerName = justClass.plus("Handler")
         val eventHandlerTemplate = FileTemplateManager.getInstance(project).getTemplate("myresourcecreatedeventhandler")
 
@@ -87,7 +90,8 @@ class ConsumeEventAction : DumbAwareAction(){
             psiDirectory = PsiManager.getInstance(project).findDirectory(directory)!!
         } else {
             //Multimodule project
-            packageName = mavenModel!!.groupId.value + "." + selectedModuleName.toLowerCase().replace("-", "") + ".events"
+            packageName =
+                mavenModel!!.groupId.value + "." + selectedModuleName.toLowerCase().replace("-", "") + ".events"
             directoryPath = packageName.replace(".", File.separator)
             val directory = VfsUtil.createDirectories(
                 project.basePath + File.separator + selectedModuleName + File.separator + File.separator + Paths.get(
@@ -112,13 +116,15 @@ class ConsumeEventAction : DumbAwareAction(){
 
         WriteCommandAction.runWriteCommandAction(project) {
             val psiJavaFile = FileTools.createFileFromTemplate(
-                eventHandlerName, eventHandlerTemplate, psiDirectory!!, "", true, HashMap(),
+                eventHandlerName, eventHandlerTemplate, psiDirectory, "", true, HashMap(),
                 properties
             )
             javaCodeStyleManager.shortenClassReferences(psiJavaFile as PsiJavaFile)
             codeStyleManager.reformat(psiJavaFile)
-            Notification("Backbase notification group", "Consume an Event", "Creating class $eventHandlerName",
-                NotificationType.INFORMATION).notify(project)
+            Notification(
+                "Backbase notification group", "Consume an Event", "Creating class $eventHandlerName",
+                NotificationType.INFORMATION
+            ).notify(project)
         }
     }
 
@@ -127,27 +133,31 @@ class ConsumeEventAction : DumbAwareAction(){
 
         val file = MavenTools.findPomXml(e.dataContext)
 
-        if(file == null) {
+        if (file == null) {
             e.presentation.isVisible = false
             return
         }
 
-        val eventClass = JavaPsiFacade.getInstance(e.project!!).findClass("com.backbase.buildingblocks.persistence.model.Event",
-            GlobalSearchScope.allScope(e.project!!))
+        val eventClass = JavaPsiFacade.getInstance(e.project!!).findClass(
+            "com.backbase.buildingblocks.persistence.model.Event",
+            GlobalSearchScope.allScope(e.project!!)
+        )
 
-        if( eventClass == null) {
+        if (eventClass == null) {
             e.presentation.isVisible = false
             return
         }
 
-        val events = ClassInheritorsSearch.search(eventClass, GlobalSearchScope.allScope(e.project!!),
-            true, true, true)
+        val events = ClassInheritorsSearch.search(
+            eventClass, GlobalSearchScope.allScope(e.project!!),
+            true, true, true
+        )
 
 //        val descendantsSearchScope = GlobalSearchScope.moduleWithDependenciesScope(e.getData(LangDataKeys.MODULE)!!)
 //        val events = ClassInheritorsSearch.search(eventClass!!, descendantsSearchScope,
 //            true, true, true)
 
-        if( !events.any()) {
+        if (!events.any()) {
             e.presentation.isVisible = false
             return
         }
