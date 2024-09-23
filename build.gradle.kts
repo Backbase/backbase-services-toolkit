8c86df27d51418052f6baa9497ace2d140e669bb
malar_backbase <malar@backbase.com>
2024-09-23 09:51:11 +0200

    using intellijPlatform extension update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -12,16 +11,14 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.24"
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij.platform") version "2.0.1"
-    // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij.platform.migration") version "2.0.1"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
+    id("org.jetbrains.qodana") version "2023.3.2"
 }
 
-group = properties("pluginGroup")
-version = properties("pluginVersion")
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
 
 // Configure project's dependencies
 repositories {
@@ -34,21 +31,20 @@ repositories {
 }
 
 
-
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-    intellijPlatform {
-    compileOnly("org.apache.maven:maven-artifact:3.9.6")
+    //compileOnly("org.apache.maven:maven-artifact:3.9.6")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    intellijPlatform {
 
-    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+        // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
 
         create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
 
-        create(IntelliJPlatformType.IntellijIdeaUltimate, providers.gradleProperty("platformVersion"))
+       // create(IntelliJPlatformType.IntellijIdeaUltimate, providers.gradleProperty("platformVersion"))
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
-        //bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
 
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
@@ -60,100 +56,98 @@ dependencies {
     }
 }
 
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    intellijPlatform {
-        groups.empty()
-        version.set(properties("pluginVersion"))
-        repositoryUrl = providers.gradleProperty("pluginRepositoryUrl") //NA
+intellijPlatform {
+
+    pluginConfiguration {
+        version = providers.gradleProperty("pluginVersion")
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild")
+        }
+    }
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+    pluginVerification {
+        ides {
+            recommended()
+        }
     }
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+
+    groups.empty()
+    version = providers.gradleProperty("pluginVersion")
+    repositoryUrl = providers.gradleProperty("pluginRepositoryUrl") //NA
+
+}
+
+/*// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
 qodana {
     cachePath.set(projectDir.resolve(".qodana").canonicalPath)
     reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
     saveReport.set(true)
     showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
-}
+}*/
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
 
 tasks {
-
-    test{
+    test {
         useJUnitPlatform()
     }
-
     wrapper {
-        gradleVersion = properties("gradleVersion")
-    }
-
-    patchPluginXml {
-        intellijPlatform {
-            version = properties("pluginVersion")
-            sinceBuild = properties("pluginSinceBuild")
-            untilBuild = properties("pluginUntilBuild")
-
-            // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-            pluginDescription.set(
-                projectDir.resolve("README.md").readText().lines().run {
-                    val start = "<!-- Plugin description -->"
-                    val end = "<!-- Plugin description end -->"
-
-                    if (!containsAll(listOf(start, end))) {
-                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                    }
-                    subList(indexOf(start) + 1, indexOf(end))
-                }.joinToString("\n").run { markdownToHTML(this) }
-            )
-
-            // Get the latest available change notes from the changelog file
-            changeNotes.set(provider {
-                changelog.renderItem(
-                    changelog.run {
-                        getOrNull(properties("pluginVersion")) ?: getLatest()
-                    },
-                    Changelog.OutputType.HTML
-                )
-            })
-        }
-    }
-
-    signPlugin {
-        intellijPlatform {
-            certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-            privateKey.set(System.getenv("PRIVATE_KEY"))
-            password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-        }
+        gradleVersion = providers.gradleProperty("gradleVersion").get()
     }
 
     publishPlugin {
-        intellijPlatform {
-            dependsOn("patchChangelog")
-            token.set(System.getenv("PUBLISH_TOKEN"))
-            // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-            // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-            // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-            channels.set(
-                listOf(
-                    properties("pluginVersion").substringAfter('-', "").substringBefore('.').ifEmpty { "default" })
-            )
-        }
+        dependsOn(patchChangelog)
     }
-
-    /*pluginVerification {
-        ides {
-            recommended()
-        }
-    }*/
-
 }
 
-tasks.register("incrementVersion"){
+
+tasks.register("incrementVersion") {
     intellijPlatform {
         fun generateVersion(version: String): String {
             val (oldMajor, oldMinor, oldPatch) = version.split(".").map(String::toInt)
@@ -171,23 +165,23 @@ tasks.register("incrementVersion"){
     }
 }
 
-/*
-val runIdeForUiTests by intellijPlatformTesting.runIde.registering {
-    task {
-        jvmArgumentProviders += CommandLineArgumentProvider {
-            listOf(
-                "-Drobot-server.port=8082",
-                "-Dide.mac.message.dialogs.as.sheets=false",
-                "-Djb.privacy.policy.text=<!--999.999-->",
-                "-Djb.consents.confirmation.enabled=false",
-            )
+intellijPlatformTesting {
+    runIde {
+        register("runIdeForUiTests") {
+            task {
+                jvmArgumentProviders += CommandLineArgumentProvider {
+                    listOf(
+                        "-Drobot-server.port=8082",
+                        "-Dide.mac.message.dialogs.as.sheets=false",
+                        "-Djb.privacy.policy.text=<!--999.999-->",
+                        "-Djb.consents.confirmation.enabled=false",
+                    )
+                }
+            }
+
+            plugins {
+                robotServerPlugin()
+            }
         }
     }
-
-   */
-/* plugins {
-        robotServerPlugin(Constraints.LATEST_VERSION)
-    }*//*
-
 }
-*/
