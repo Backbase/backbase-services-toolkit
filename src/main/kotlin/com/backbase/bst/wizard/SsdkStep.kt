fce64ab34b60c9c957f19d2dfa9f9b974d3ead67
malara <amalarsri@gmail.com>
2024-10-03 16:08:39 +0200

    Combobox upgrade

--- src/main/kotlin/com/backbase/bst/wizard/SsdkStep.kt
+++ src/main/kotlin/com/backbase/bst/wizard/SsdkStep.kt
@@ -3,6 +3,7 @@ package com.backbase.bst.wizard
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.bindItem
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.idea.maven.model.MavenId
@@ -36,8 +37,9 @@ class SsdkStep(
         }
         // Row for SSDK Version ComboBox
         row("SSDK Version:") {
-            comboBox(DefaultComboBoxModel(ssdkVersions.toTypedArray()))
-            //.bindSelectedItemIn(projectId::ssdkVersion)
+            comboBox(DefaultComboBoxModel(ssdkVersions.toTypedArray())).bindItem(
+                projectId::ssdkVersion,
+                { selectedOption -> projectId.ssdkVersion = selectedOption!! })
         }
 
     }
--- src/main/kotlin/com/backbase/bst/wizard/extensions/ExtensionProjectStep.kt
+++ src/main/kotlin/com/backbase/bst/wizard/extensions/ExtensionProjectStep.kt
@@ -4,9 +4,11 @@ import com.backbase.bst.common.SsdkUtils
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.dsl.builder.bindItem
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.idea.maven.model.MavenId
+import javax.swing.DefaultComboBoxModel
 import javax.swing.JComponent
 
 
@@ -22,17 +24,23 @@ class ExtensionProjectStep(
 
     private val panel: DialogPanel = panel {
 
-        row{
+        row {
             textField().bindText(::groupId).label("GroupId").comment("The groupId of the generated Maven project")
         }
         row {
-            textField().bindText(::artifactId).label("ArtifactId").comment("The artifactId of the generated Maven project")
+            textField().bindText(::artifactId).label("ArtifactId")
+                .comment("The artifactId of the generated Maven project")
         }
         row {
-            textField().bindText(::version).label("Version").comment("The version number of the generated Maven project")
+            textField().bindText(::version).label("Version")
+                .comment("The version number of the generated Maven project")
         }
         row {
-            comboBox(SsdkUtils.listVersionsSsdk()).label("Extension Version").comment("Backbase service extension starter parent version ")// TODO
+            comboBox(DefaultComboBoxModel(SsdkUtils.listVersionsSsdk().toTypedArray()))
+                .label("Extension Version")
+                .comment("Backbase service extension starter parent version ")
+                .bindItem(::ssdkVersion,
+                    { selectedOption -> ssdkVersion = selectedOption!! })
         }
     }
 
@@ -48,6 +56,6 @@ class ExtensionProjectStep(
         behaviourExtensionsProjectWizard.version = version
         behaviourExtensionsProjectWizard.ssdkVersion = ssdkVersion
         wizardContext.projectName = artifactId
-        behaviourExtensionsProjectWizard.myProjectId = MavenId(groupId,artifactId,version)
+        behaviourExtensionsProjectWizard.myProjectId = MavenId(groupId, artifactId, version)
     }
 }
